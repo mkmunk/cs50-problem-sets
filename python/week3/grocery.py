6237d54db8b560055144e8671d5b74bd3950f2e4
MunK <Orz824567@gmail.com>
2026-07-08 20:18:25 +0800

    completely week3 grocery

---
+++ python/week3/grocery.py
@@ -0,0 +1,14 @@
+grocery = {}
+
+while True:
+    try:
+        item = input().strip().upper()
+        if item in grocery:
+            grocery[item] += 1
+        else:
+            grocery[item] = 1
+    except EOFError:
+        break
+
+for item in sorted(grocery):
+    print(grocery[item], item)
